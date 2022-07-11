28e3255535c38b03ca04b9a8c642e4f78ba96cd9
Javier Torrón Díaz <javier.torron.diaz@gmail.com>
2022-07-11 16:35:37 +0200

    Most basic guessing name (commented)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+// Main function of the app
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();          // Mutable variable to store user input
+
+    io::stdin()
+        .read_line(&mut guess)              // Read from keyboard and store it on guess variable (parameter is a pointer)
+        .expect("Failed to read line");     // On error throw exception
+
+    println!("You guessed: {guess}");       // Print the result
 }
